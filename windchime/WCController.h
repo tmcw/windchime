43464196120b824257bddce3042e0d5902da51e1
Tom MacWright <tom@macwright.org>
2012-10-02 23:33:11 -0400

    Scales, instruments, volume. Tons of progress.

--- windchime/WCController.h
+++ windchime/WCController.h
@@ -7,8 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <AudioUnit/AudioUnit.h>
 
 @interface WCController : NSObject {
+    AudioUnit synthUnit;
+    int volume;
+    NSString *currentScale;
+    NSArray *currentNotes;
 }
 
 // UI
@@ -17,10 +22,14 @@
 @property (strong) NSMenuItem *quitMI;
 @property (strong) NSMenuItem *aboutMI;
 @property (strong) NSMenuItem *exportMI;
+@property (strong) NSMenuItem *scaleMI;
+@property (strong) NSMenuItem *volumeMI;
 @property (strong) NSImage *tiny;
-
 @property (strong) NSArray *instruments;
+@property (strong) NSDictionary *scales;
 @property (strong) NSMenuItem *instrumentMI;
 @property (strong) NSMenu *instrumentMenu;
+@property (strong) NSMenu *scaleMenu;
+@property (strong) NSMenu *volumeMenu;
 
 @end
--- windchime/WCController.m
+++ windchime/WCController.m
@@ -26,7 +26,25 @@ enum {
     self = [super init];
     if (self)
     {
-        self.instruments = [NSArray arrayWithObjects:@"Acoustic Grand Piano", @"Bright Acoustic Piano",
+        self.scales = [[NSDictionary alloc] initWithObjectsAndKeys:
+                       @[@24,@27,@31,@34,@36,@39,@43,@46,@48,@51,@55,@58,@60,@63,@67,@70,@72,@75,@79,@82,@84,@87,@91,@94,@96,@99,@103,@106,@108,@111,@115,@118],@"BiYu"
+                       , @[@41,@42,@43,@46,@48,@51,@53,@54,@55,@58,@60,@63,@65,@66,@67,@70,@72,@75,@77,@78,@79,@82,@84,@87,@89,@90,@91,@94,@96,@99,@101,@102],@"Blues"
+                       , @[@48,@49,@51,@52,@54,@55,@56,@58,@60,@61,@63,@64,@66,@67,@68,@70,@72,@73,@75,@76,@78,@79,@80,@82,@84,@85,@87,@88,@90,@91,@92,@94],@"Blues Diminished"
+                       , @[@25,@27,@30,@32,@34,@37,@39,@42,@44,@46,@49,@51,@54,@56,@58,@61,@63,@66,@68,@70,@73,@75,@78,@80,@82,@85,@87,@90,@92,@94,@97,@99],@"Dorian"
+                       , @[@51,@53,@55,@56,@57,@58,@59,@60,@62,@63,@65,@67,@68,@69,@70,@71,@72,@74,@75,@77,@79,@80,@81,@82,@83,@84,@86,@87,@89,@91,@92,@93],@"FullMinor"
+                       , @[@44,@47,@48,@50,@52,@53,@55,@56,@59,@60,@62,@64,@65,@67,@68,@71,@72,@74,@76,@77,@79,@80,@83,@84,@86,@88,@89,@91,@92,@95,@96,@98],@"HarmonicMajor"
+                       , @[@39,@43,@45,@47,@48,@50,@51,@55,@57,@59,@60,@62,@63,@67,@69,@71,@72,@74,@75,@79,@81,@83,@84,@86,@87,@91,@93,@95,@96,@98,@99,@103],@"Hawaiian"
+                       , @[@45,@47,@48,@50,@52,@53,@56,@57,@59,@60,@62,@64,@65,@68,@69,@71,@72,@74,@76,@77,@80,@81,@83,@84,@86,@88,@89,@92,@93,@95,@96,@98],@"Ionian #5"
+                       , @[@45,@47,@48,@50,@51,@53,@55,@57,@59,@60,@62,@63,@65,@67,@69,@71,@72,@74,@75,@77,@79,@81,@83,@84,@86,@87,@89,@91,@93,@95,@96,@98],@"Jazz Minor"
+                       , @[@45,@47,@48,@50,@52,@54,@55,@57,@59,@60,@62,@64,@66,@67,@69,@71,@72,@74,@76,@78,@79,@81,@83,@84,@86,@88,@90,@91,@93,@95,@96,@98],@"Lydian"
+                       , @[@43,@45,@48,@50,@51,@52,@54,@55,@57,@60,@62,@63,@64,@66,@67,@69,@72,@74,@75,@76,@78,@79,@81,@84,@86,@87,@88,@90,@91,@93,@96,@98],@"Major"
+                       , @[@45,@46,@48,@50,@52,@53,@55,@57,@58,@60,@62,@64,@65,@67,@69,@70,@72,@74,@76,@77,@79,@81,@82,@84,@86,@88,@89,@91,@93,@94,@96,@98],@"Mixolydian"
+                       , @[@45,@46,@48,@49,@52,@53,@54,@57,@58,@60,@61,@64,@65,@66,@69,@70,@72,@73,@76,@77,@78,@81,@82,@84,@85,@88,@89,@90,@93,@94,@96,@97],@"Oriental"
+                       , @[@44,@46,@48,@49,@51,@52,@54,@56,@58,@60,@61,@63,@64,@66,@68,@70,@72,@73,@75,@76,@78,@80,@82,@84,@85,@87,@88,@90,@92,@94,@96,@97],@"Super Locrian"
+                       , @[@46,@47,@48,@49,@52,@54,@56,@58,@59,@60,@61,@64,@66,@68,@70,@71,@72,@73,@76,@78,@80,@82,@83,@84,@85,@88,@90,@92,@94,@95,@96,@97],@"Verdi Enigmatic Ascending"
+                       , @[@48,@50,@51,@53,@55,@56,@57,@59,@60,@62,@63,@65,@67,@68,@69,@71,@72,@74,@75,@77,@79,@80,@81,@83,@84,@86,@87,@89,@91,@92,@93,@95],@"Zirafkend",nil
+                               ];
+        self.instruments = @[@"Acoustic Grand Piano", @"Bright Acoustic Piano",
            @"Electric Grand Piano",@"Honky-tonk Piano",
            @"Electric Piano 1",@"Electric Piano 2",@"Harpsichord",
            @"Clavi",@"Celesta",@"Glockenspiel",@"Music Box",
@@ -66,8 +84,8 @@ enum {
            @"Woodblock",@"Taiko Drum",@"Melodic Tom",@"Synth Drum",
            @"Reverse Cymbal",@"Guitar Fret Noise",@"Breath Noise",
            @"Seashore",@"Bird Tweet",@"Telephone Ring",
-           @"Helicopter",@"Applause",@"Gunshot",nil];
-        
+           @"Helicopter",@"Applause",@"Gunshot"];
+         
         NSString* tinyName = [[NSBundle mainBundle]
                               pathForResource:@"windchime"
                               ofType:@"png"];
@@ -83,17 +101,39 @@ enum {
         [self.statusItem setImage:self.tiny];
         [self.statusItem setHighlightMode:YES];
         
-        
         self.instrumentMenu = [[NSMenu alloc] init];
         
-        [self.instrumentMenu addItemWithTitle:@"Sub item" action:nil keyEquivalent:@""];
+        unsigned int idx = 0;
+        for (id item in self.instruments) {
+            id it = [self.instrumentMenu addItemWithTitle:item
+                                           action:@selector(setInstrument:)
+                                                    keyEquivalent:@""];
+            [it setTarget:self];
+            [it setRepresentedObject:[NSNumber numberWithUnsignedInt: idx]];
+            idx++;
+        }
+        
+        
+        self.scaleMenu = [[NSMenu alloc] init];
+        for (id item in self.scales) {
+            NSLog(@"item: @%", item);
+            id it = [self.scaleMenu addItemWithTitle:item
+                                                   action:@selector(setScale:)
+                                            keyEquivalent:@""];
+            [it setTarget:self];
+            [it setRepresentedObject:item];
+        }
         
         self.instrumentMI = [[NSMenuItem alloc] init];
         [self.instrumentMI setTitle:NSLocalizedString(@"Instruments",@"")];
-        
         [self.instrumentMI setSubmenu:self.instrumentMenu];
-        
         [self.instrumentMI setTarget:self];
+        
+        
+        self.scaleMI = [[NSMenuItem alloc] init];
+        [self.scaleMI setTitle:NSLocalizedString(@"Scale (Dorian)",@"")];
+        [self.scaleMI setSubmenu:self.scaleMenu];
+        [self.scaleMI setTarget:self];
         
         self.aboutMI = [[NSMenuItem alloc]
                         initWithTitle:NSLocalizedString(@"About",@"")
@@ -103,29 +143,38 @@ enum {
         
         // Set up the menu
         self.quitMI = [[NSMenuItem alloc]
-                   initWithTitle:NSLocalizedString(@"Quit",@"") 
+                   initWithTitle:NSLocalizedString(@"Quit",@"")
                    action:@selector(terminate:) 
                   keyEquivalent:@""];
         
+        self.volumeMenu = [[NSMenu alloc] init];
+        self.volumeMI = [[NSMenuItem alloc] initWithTitle:@"Volume (1)" action:nil keyEquivalent:@""];
+        
+        [self.volumeMI setSubmenu:self.volumeMenu];
+        for (int vol = 1; vol <= 5; vol++) {
+            id it = [self.volumeMenu addItemWithTitle:[NSString stringWithFormat:@"%d", vol]
+                                                   action:@selector(setVolume:)
+                                            keyEquivalent:@""];
+            [it setTarget:self];
+            [it setRepresentedObject:[NSNumber numberWithInt: vol]];
+            idx++;
+        }
+        
         [self.menu addItem:self.aboutMI];
         [self.menu addItem:self.instrumentMI];
+        [self.menu addItem:self.scaleMI];
+        [self.menu addItem:self.volumeMI];
         [self.menu addItem:self.quitMI];
     }
 
-    AudioUnit synthUnit;
-    // Audio Unit graph
     AUGraph graph;
-    // Audio Unit synthesizer and output node
     AUNode synthNode;
     AUNode outNode;
-    // Component description
     ComponentDescription cd;
     cd.componentManufacturer = kAudioUnitManufacturer_Apple;
     cd.componentFlags = 0;
     cd.componentFlagsMask = 0;
-    // New AU graph
     NewAUGraph(&graph);
-    // Synthesizer
     cd.componentType = kAudioUnitType_MusicDevice;
     cd.componentSubType = kAudioUnitSubType_DLSSynth;
     // New synthesizer node
@@ -145,8 +194,9 @@ enum {
     AUGraphInitialize(graph);
     // Start
     AUGraphStart(graph);
-    // Change instrument
-    int instrument = 9;
+    int instrument = 12;
+    self->currentScale = @"Dorian";
+    self->currentNotes = [self.scales valueForKey:self->currentScale];
     
     MusicDeviceMIDIEvent(synthUnit, kMidiMessageProgramChange + 0,
                          instrument, 0, 0);
@@ -156,6 +206,12 @@ enum {
                          instrument, 0, 0);
     
     __block int level = 3;
+    __block int lastnote = 0;
+    self->volume = 10;
+
+
+    //int jazzminor[32] = {45,47,48,50,51,53,55,57,59,60,62,63,65,67,69,71,72,74,75,77,79,81,83,84,86,87,89,91,93,95,96,98};
+    //int lydian[32] = {45,47,48,50,52,54,55,57,59,60,62,64,66,67,69,71,72,74,76,78,79,81,83,84,86,88,90,91,93,95,96,98};
     
     /*
      http://www.lawriecape.co.uk/theblog/index.php/archives/881
@@ -167,7 +223,6 @@ enum {
          int jazzminor[32] = {45,47,48,50,51,53,55,57,59,60,62,63,65,67,69,71,72,74,75,77,79,81,83,84,86,87,89,91,93,95,96,98};
          int lydian[32] = {45,47,48,50,52,54,55,57,59,60,62,64,66,67,69,71,72,74,76,78,79,81,83,84,86,88,90,91,93,95,96,98};
          int note = 0;
-         int volume = 10;
          if ([[event characters] isEqualToString:@"["] ||
              [[event characters] isEqualToString:@"{"] ||
              [[event characters] isEqualToString:@"("]) {
@@ -177,20 +232,51 @@ enum {
                   [[event characters] isEqualToString:@")"]) {
              level--;
          }
-         if (level < 1) { level = 1; }
-         if (level > 5) { level = 5; }
+         if (level < 0) { level = 0; }
+         if (level > 1) { level = 1; }
          
          int idx = (int) [[event characters] characterAtIndex:0] % 32;
-         note = lydian[idx] + (12 * level);
+         note = [[self->currentNotes objectAtIndex:idx] intValue] + (12 * level);
+         
+         MusicDeviceMIDIEvent(synthUnit,
+                              kMidiMessageNoteOff,
+                              lastnote, self->volume, 0);
+         
          MusicDeviceMIDIEvent(synthUnit,
                               kMidiMessageNoteOn,
-                              note, volume, 0);
+                              note, self->volume, 0);
+         
+         lastnote = note;
      }];
     return self;
 }
 
+- (void)setInstrument:sender
+{
+    int instrument = [[sender representedObject] intValue];
+    
+    MusicDeviceMIDIEvent(self->synthUnit, kMidiMessageProgramChange + 0,
+                         instrument, 0, 0);
+    MusicDeviceMIDIEvent(self->synthUnit, kMidiMessageProgramChange + 1,
+                         instrument, 0, 0);
+    MusicDeviceMIDIEvent(self->synthUnit, kMidiMessageProgramChange + 2,
+                         instrument, 0, 0);
+}
 
-- (void)about
+- (void)setScale:sender
+{
+    self->currentScale = [sender representedObject];
+    [self.scaleMI setTitle:[NSString stringWithFormat:@"Scale (%@)", self->currentScale]];
+    self->currentNotes = [self.scales valueForKey:self->currentScale];
+}
+
+- (void)setVolume:sender
+{
+    self->volume = [[sender representedObject] intValue] * 20;
+    [self.volumeMI setTitle:[NSString stringWithFormat:@"Volume (%d)", [[sender representedObject] intValue]]];
+}
+
+- (IBAction)about
 {
     NSLog(@"Showing about window");
     NSApplication *app = [NSApplication sharedApplication];
